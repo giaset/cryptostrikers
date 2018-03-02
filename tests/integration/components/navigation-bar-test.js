2428775ccd364f23b51953e99172e25f643254f3
Gianni Settino <g@miltonandparc.com>
2018-03-02 02:23:54 -0500

    Adds navigation-bar component

---
+++ tests/integration/components/navigation-bar-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | navigation-bar', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{navigation-bar}}`);
+
+    //assert.equal(this.element.textContent.trim(), '');
+    assert.ok(true);
+  });
+});
