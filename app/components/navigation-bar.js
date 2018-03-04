fe62098dbc44bd291e5e7a1ea51737534e120d92
Gianni Settino <g@miltonandparc.com>
2018-03-04 02:15:32 -0500

    Start working on landing page

--- app/components/navigation-bar.js
+++ app/components/navigation-bar.js
@@ -2,5 +2,5 @@ import Component from '@ember/component';
 
 export default Component.extend({
   tagName: 'header',
-  classNames: ['navbar', 'navbar-expand-sm', 'navbar-light', 'border-bottom', 'box-shadow']
+  classNames: ['navbar', 'navbar-expand-sm', 'navbar-light']
 });
