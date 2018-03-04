efc750c0d261c86b219a45450ebd3b121963a7a8
Gianni Settino <g@miltonandparc.com>
2018-03-03 22:21:30 -0500

    Basic setup of Album page

--- app/routes/album.js
+++ app/routes/album.js
@@ -1,4 +1,38 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return[
+      {
+        name: 'Lionel Messi'
+      },
+      {
+        name: 'Neymar'
+      },
+      {
+        name: 'Cristiano Ronaldo'
+      },
+      {
+        name: 'Kylian Mbappe'
+      },
+      {
+        name: 'Paul Pogba'
+      },
+      {
+        name: 'Gianluigi Buffon'
+      },
+      {
+        name: 'Alexis Sanchez'
+      },
+      {
+        name: 'Zlatan Ibrahimovic'
+      },
+      {
+        name: 'Harry Kane'
+      },
+      {
+        name: 'Jozy Altidore'
+      }
+    ];
+  }
 });
