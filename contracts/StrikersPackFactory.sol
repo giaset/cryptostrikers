cdb4e9e0383007a075638894a27d4011c3304def
Gianni Settino <g@miltonandparc.com>
2018-03-24 12:45:53 -0400

    Use uint32 to represent packs in storage

--- contracts/StrikersPackFactory.sol
+++ contracts/StrikersPackFactory.sol
@@ -2,7 +2,19 @@ pragma solidity ^0.4.19;
 
 import "./StrikersBase.sol";
 
+/// @dev The contract that allows the contract owner to load shuffled packs of cards
 contract StrikersPackFactory is StrikersBase {
+
+  // The only way to buy Series 1 cards is in packs of 4.
+  // NB: we generate and store the shuffled packs in advance but only actually mint the cards when a pack is sold (see PackSale contract).
+  // To save on storage, we use uint32 to represent a pack, with each of the 4 groups of 8 bits representing a playerId (see WorldCupInfo contract).
+  // For example:
+  // Pack = 00011000000001100000001000010010
+  // Card 1 = 00011000 = playerId 24
+  // Card 2 = 00000110 = playerId 6
+  // Card 3 = 00000010 = playerId 2
+  // Card 4 = 00010010 = playerId 18
+
   event RunMinted(uint8 runNumber);
 
   // The number of cards in a pack
@@ -18,19 +30,19 @@ contract StrikersPackFactory is StrikersBase {
 
   mapping (uint8 => uint32) public runNumberToRunSize;
 
-  mapping (uint8 => mapping (uint8 => uint16)) public playerCountForRun;
+  //mapping (uint8 => mapping (uint8 => uint16)) public playerCountForRun;
 
   // Maybe this shouldn't be public
-  uint8[][] public packs;
+  uint32[] public shuffledPacks;
 
   // Series 1 only
-  function mintRun(uint8[PACK_SIZE][] _shuffledPacks) external onlyOwner {
+  function loadShuffledPacks(uint32[] _shuffledPacks) external onlyOwner {
     uint32 runSize = uint32(_shuffledPacks.length) * PACK_SIZE;
-    require (packsMinted + runSize <= PACKS_MINTED_LIMIT);
+    require(packsMinted + runSize <= PACKS_MINTED_LIMIT);
     packsMinted += runSize;
     uint8 runNumber = currentRunNumber++;
     runNumberToRunSize[runNumber] = runSize;
-    packs = _shuffledPacks;
+    shuffledPacks = _shuffledPacks;
     RunMinted(runNumber);
   }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,18 +2,20 @@ const StrikersPackFactory = artifacts.require("./StrikersPackFactory.sol");
 
 module.exports = function(deployer) {
   const packs = [];
-  for (let i = 0; i < 30; i++) {
-    const pack = [];
+  for (let i = 0; i < 60; i++) {
+    let pack = '';
     for (let j = 0; j < 4; j++) {
       const playerId = Math.floor((Math.random() * 24) + 1);
-      pack.push(playerId);
+      const binaryPlayerId = playerId.toString(2);
+      const paddedBinaryPlayerId = '00000000'.substr(binaryPlayerId.length) + binaryPlayerId;
+      pack += paddedBinaryPlayerId;
     }
-    packs.push(pack);
+    packs.push(parseInt(pack, 2));
   }
 
   deployer.deploy(StrikersPackFactory)
   .then(() => StrikersPackFactory.deployed())
   .then(packFactory => {
-    packFactory.mintRun(packs);
+    packFactory.loadShuffledPacks(packs);
   });
 };
