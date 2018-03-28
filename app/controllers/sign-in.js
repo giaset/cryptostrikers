0aa52154a7d96e4fe5bad888f554a84fef5b93a4
Gianni Settino <g@miltonandparc.com>
2018-03-28 12:33:32 -0400

    Makes sign request using ember-ajax

--- app/controllers/sign-in.js
+++ app/controllers/sign-in.js
@@ -2,12 +2,24 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  ajax: service(),
   metamaskWatcher: service(),
   web3: service(),
   actions: {
     submit() {
       const currentAccount = this.get('metamaskWatcher.currentAccount');
-      this.get('web3').personalSign('CryptoStrikers', currentAccount);
+      this.get('web3').personalSign('CryptoStrikers', currentAccount)
+        .then(signature => {
+          return this.get('ajax').post('sign', {
+            data: {
+              address: currentAccount,
+              signature: signature
+            }
+          });
+        })
+        .then(res => {
+          this.set('token', res.token);
+        });
     }
   }
 });
