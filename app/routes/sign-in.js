c5f9e55eb008b1a363ab642457a9c75e9b9baaba
Gianni Settino <g@miltonandparc.com>
2018-03-29 12:02:55 -0400

    Makes sign-in an unauthenticated route

--- app/routes/sign-in.js
+++ app/routes/sign-in.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  beforeModel() {
+    if (this.get('session.isAuthenticated')) {
+      this.transitionTo('my-album');
+    } else {
+      return this._super(...arguments);
+    }
+  }
 });
