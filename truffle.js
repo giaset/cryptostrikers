159bea42ef6ec3b3225956d0fc748235f44c4831
Gianni Settino <g@miltonandparc.com>
2018-03-30 22:38:49 -0400

    Truffle config for rinkeby

--- truffle.js
+++ truffle.js
@@ -1,9 +1,20 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const mnemonic = require('./.mnemonic');
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic.seed, 'https://rinkeby.infura.io/b9XMoJFDxpzhBpEGzVaW', 4);
+      },
+      network_id: 4,
+      gas: 7000000
     }
   }
 };
