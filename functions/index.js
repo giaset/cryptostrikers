95bca18a76032a7ba310e8963b7a2e9e703a72ad
Gianni Settino <g@miltonandparc.com>
2018-04-03 22:19:55 -0400

    Adds sign function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
+const admin = require('firebase-admin');
+const cors = require('cors')({origin: true});
 const functions = require('firebase-functions');
+const serviceAccount = require(functions.config().strikers.service_account);
+const sigUtil = require('eth-sig-util');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: functions.config().strikers.database_url
+});
+
+exports.sign = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    const address = req.body.address;
+    const signature = req.body.signature;
+    if (address && signature) {
+      const msgParams = {data: 'CryptoStrikers', sig: signature};
+      const recovered = sigUtil.recoverPersonalSignature(msgParams);
+      if (recovered === sigUtil.normalize(address)) {
+        admin.auth().createCustomToken(address)
+          .then(customToken => res.json({token: customToken}))
+          .catch(error => res.json({error: error}));
+      } else {
+        res.status(401).send('Wrong signature for address.');
+      }
+    } else {
+      res.status(500).send('Invalid address and signature params.');
+    }
+  });
+});
