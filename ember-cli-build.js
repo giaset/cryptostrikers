d087c2b291c7df9555ac7e16ebd649fc5243abeb
Gianni Settino <g@miltonandparc.com>
2018-04-06 01:10:10 -0400

    Fix for cache busting

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    fingerprint: {
+      exclude: ['/cards/']
+    },
     sassOptions: {
       includePaths: [
         'vendor/bootstrap/scss'
