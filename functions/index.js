bca4f923b57208a52793fc82938bae481789e0e3
Gianni Settino <g@miltonandparc.com>
2018-04-11 12:34:58 -0400

    Progress on /cards/ API

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,34 @@
 const admin = require('firebase-admin');
+const app = require('express')();
+const contractJson = require('./StrikersMinting');
 const cors = require('cors')({origin: true});
 const functions = require('firebase-functions');
 const serviceAccount = require(functions.config().strikers.service_account);
 const sigUtil = require('eth-sig-util');
+const Web3 = require('web3');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: functions.config().strikers.database_url
 });
+
+// TODO: use config for mainnet infura and contract address
+// mainnet: https://mainnet.infura.io/b9XMoJFDxpzhBpEGzVaW
+const web3 = new Web3('https://rinkeby.infura.io/b9XMoJFDxpzhBpEGzVaW');
+const address = '0xAfA2D5AdB646DD90424f522BA8EE8cc118534c48';
+const strikersContract = new web3.eth.Contract(contractJson.abi, address);
+
+app.use(cors);
+app.get('/:id', (req, res) => {
+  const cardId = req.params.id;
+  strikersContract.methods.cards(cardId).call().then(card => {
+    return res.json(card);
+  })
+  .catch(error => {
+    res.status(404).send('Card not found');
+  });
+});
+exports.cards = functions.https.onRequest(app);
 
 exports.sign = functions.https.onRequest((req, res) => {
   return cors(req, res, () => {
