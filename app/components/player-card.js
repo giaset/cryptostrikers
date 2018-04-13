beaa384ee85170a1517784337040e15c08e9b4d8
Gianni Settino <g@miltonandparc.com>
2018-04-12 20:55:18 -0400

    Import Mau's new cards

--- app/components/player-card.js
+++ app/components/player-card.js
@@ -4,11 +4,6 @@ import { computed } from '@ember/object';
 export default Component.extend({
   classNames: ['player-card', 'card', 'mb-4', 'box-shadow'],
 
-  hasImage: computed('safePlayer.id', function() {
-    const noImage = ['15', '16', '17', '18', '20', '21', '22', '23', '24'];
-    return !noImage.includes(this.get('safePlayer.id'));
-  }),
-
   safePlayer: computed('card.player', 'player', function() {
     return this.get('card.player') || this.get('player');
   })
