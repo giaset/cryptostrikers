0a1c64d8b0fcf4f7a412f59120cd2667518f4750
Gianni Settino <g@miltonandparc.com>
2018-04-29 23:08:15 -0400

    Disable sales once the countdown is done

--- app/components/pack-sale-tile.js
+++ app/components/pack-sale-tile.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 import moment from 'moment';
 
 export default Component.extend({
+  classNameBindings: ['countdownOver:sale-over'],
   classNames: ['pack-sale-tile', 'my-3', 'p-3', 'rounded', 'border', 'box-shadow', 'd-flex'],
   modal: service(),
   selectedQuantity: 1,
@@ -20,11 +21,28 @@ export default Component.extend({
     return (qty === 1) ? 'Buy 1 Pack' : `Buy ${qty} Packs`;
   }),
 
-  countdownString: computed('clock.second', 'sale.endTime', function() {
+  countdownDiff: computed('clock.second', 'sale.endTime', function() {
+    const endTime = this.get('sale.endTime');
+    if (!endTime) { return; }
     const now = new Date().getTime();
-    const endTime = this.get('sale.endTime').getTime();
-    const diff = endTime - now;
+    return endTime.getTime() - now;
+  }),
 
+  countdownString: computed('countdownDiff', function() {
+    const diff = this.get('countdownDiff');
+    return this._countdownStringForDiff(diff);
+  }),
+
+  // TODO: maybe need to watch the pack bought event and update this in real time?
+  // 'sale.{endTime,packsOffered,packsSold,state}'
+  // state === 1, return false
+  countdownOver: computed('countdownDiff', function() {
+    const diff = this.get('countdownDiff');
+    if (!diff) { return false; }
+    return diff <= 0;
+  }),
+
+  _countdownStringForDiff(diff) {
     if (diff < 0) {
       return '00:00:00';
     }
@@ -34,19 +52,11 @@ export default Component.extend({
     const minutes = this._padLeft(duration.minutes());
     const seconds = this._padLeft(duration.seconds());
     return `${hours}:${minutes}:${seconds}`;
-  }),
+  },
 
   _padLeft(value) {
     // '0' + '12' = '012', '012'.slice(-2) = '12'
     // '0' + '1' = '01', '01'.slice(-2) = '01'
     return ('0' + value).slice(-2);
-  }/*,
-
-  // TODO: maybe need to watch the pack bought event and update this in real time?
-  isActive: computed('clock.second', 'sale.{endTime,packsOffered,packsSold,state}', function() {
-    if (!this.get('state') === 1) {
-      return false;
-    }
-
-  })*/
+  }
 });
