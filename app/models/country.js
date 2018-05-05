4695d4c134fa8c453494fdd4b005f56fcbd65430
Gianni Settino <g@miltonandparc.com>
2018-05-05 15:36:57 -0400

    Adds country-selector component

--- app/models/country.js
+++ app/models/country.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   group: DS.attr('string'),
+  isoCode: DS.attr('string'),
   name: DS.attr('string')
 });
--- app/models/player.js
+++ app/models/player.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  country: DS.belongsTo(),
+  country: DS.belongsTo('country', { inverse: null }),
   name: DS.attr('string')
 });
