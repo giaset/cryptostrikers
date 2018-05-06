9ad8a66d4669bb1e3739a18523d2f2175dbecc94
Gianni Settino <g@miltonandparc.com>
2018-05-06 11:04:58 -0400

    Adds card-tile component

---
+++ tests/integration/components/card-tile-test.js
@@ -0,0 +1,30 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | card-tile', function(hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
+    this.owner.lookup('service:clock').stop();
+  });
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{card-tile}}`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    await render(hbs`
+      {{#card-tile}}
+        template block text
+      {{/card-tile}}
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+});
