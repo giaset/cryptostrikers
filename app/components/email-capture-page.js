2bb6bee87372f20c33ac006ff93bea90588d1ab9
Gianni Settino <g@miltonandparc.com>
2018-05-22 05:46:13 -0400

    Wire up new email submission

--- app/components/email-capture-page.js
+++ app/components/email-capture-page.js
@@ -1,5 +1,23 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
 
 export default Component.extend({
-  classNames: ['email-capture-page']
+  classNames: ['email-capture-page'],
+  store: service(),
+  actions: {
+    createLead(emailAddress) {
+      if (!this.get('leadCreated')) {
+        this.get('createLeadTask').perform(emailAddress);
+      }
+    }
+  },
+
+  createLeadTask: task(function * (emailAddress) {
+    const newLead = this.get('store').createRecord('lead', {
+      email: emailAddress
+    });
+    yield newLead.save();
+    this.set('leadCreated', true);
+  }).drop()
 });
