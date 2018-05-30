e4dd0b4455a17b5a2b11a9174e1d4ef2b36e79f4
Gianni Settino <g@miltonandparc.com>
2018-05-30 00:02:33 -0400

    Fix export-checklist

--- helpers/export-checklist.js
+++ helpers/export-checklist.js
@@ -1,35 +1,47 @@
-const contractJson = require('../build/contracts/Checklist');
+const contractJson = require('../build/contracts/StrikersChecklist');
 const fs = require('fs');
 const Web3 = require('web3');
 
 const web3 = new Web3('http://127.0.0.1:8545');
 const contractAddress = '0x9414329bf6837db915b4d5e0e22ecc27a33129c5';
-const Checklist = new web3.eth.Contract(contractJson.abi, contractAddress).methods;
+const Checklist = new web3.eth.Contract(contractJson.abi, contractAddress);
 exportChecklist();
 exportPlayers();
 
 function exportChecklist() {
-  const BASE_SET_COUNT = 25;
-  const GILANG_SET_COUNT = 20;
-  const promises = [];
-  for (let i = 0; i < BASE_SET_COUNT; i++) {
-    promises.push(Checklist.setIdToChecklistItems(0, i).call());
-  }
-  for (let i = 0; i < GILANG_SET_COUNT; i++) {
-    promises.push(Checklist.setIdToChecklistItems(1, i).call());
-  }
-  return Promise.all(promises).then(checklistItems => {
+  const contract = Checklist.methods;
+  const countPromises = [
+    contract.originalsCount().call(),
+    contract.iconicsCount().call()
+  ];
+
+  let originalsCount;
+  let iconicsCount;
+  Promise.all(countPromises)
+  .then(counts => {
+    originalsCount = parseInt(counts[0]);
+    iconicsCount = parseInt(counts[1]);
+    const promises = [];
+    for (let i = 0; i < originalsCount; i++) {
+      promises.push(contract.originalChecklistItems(i).call());
+    }
+    for (let i = 0; i < iconicsCount; i++) {
+      promises.push(contract.iconicChecklistItems(i).call());
+    }
+    return Promise.all(promises);
+  })
+  .then(checklistItems => {
     const result = {};
     checklistItems.forEach((checklistItem, index) => {
-      const isGilang = index > (BASE_SET_COUNT - 1);
-      const setId = isGilang ? '1' : '0';
-      const id = isGilang ? (index - BASE_SET_COUNT) : index;
+      const isIconic = index >= originalsCount;
+      const setId = isIconic ? '1' : '0';
+      const id = isIconic ? (index - originalsCount) : index;
       const paddedId = ('0' + id).slice(-2);
       const checklistId = setId + paddedId;
       result[checklistId] = {
+        checklistSet: setId,
         player: checklistItem.playerId,
-        set: setId,
-        totalIssuance: checklistItem.totalIssuance
+        tier: checklistItem.tier
       };
     });
     fs.writeFileSync('checklist.json', JSON.stringify(result, null, 2));
@@ -37,18 +49,9 @@ function exportChecklist() {
 }
 
 function exportPlayers() {
-  const PLAYER_COUNT = 50;
-  const promises = [];
-  for (let i = 0; i < PLAYER_COUNT; i++) {
-    promises.push(Checklist.players(i).call());
-  }
-  return Promise.all(promises).then(players => {
-    const result = players.map(player => {
-      return {
-        country: player.countryId,
-        name: player.fullName
-      };
-    });
-    fs.writeFileSync('players.json', JSON.stringify(result, null, 2));
+  Checklist.getPastEvents('PlayerAdded', { fromBlock: 0, toBlock: 'latest' })
+  .then(events => {
+    const players = events.map(event => ({ name: event.returnValues.name, country: '0' }));
+    fs.writeFileSync('players.json', JSON.stringify(players, null, 2));
   });
 }
