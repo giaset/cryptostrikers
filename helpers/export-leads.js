cc67d8cbea2ff8562b98fb67296710e8c5a44b3c
Gianni Settino <g@miltonandparc.com>
2018-06-01 05:35:23 -0400

    Delete useless helpers

--- helpers/export-leads.js
+++
@@ -1,4 +0,0 @@
-const leadsJson = require('./cryptostrikers-prod-leads-export');
-const allEmails = Object.values(leadsJson).map(lead => lead.email);
-const uniqEmails = Array.from(new Set(allEmails));
-console.log(uniqEmails);
