a0e85310d9c8cc43d04e95c531571237e4981a71
Gianni Settino <g@miltonandparc.com>
2018-06-02 00:46:24 -0400

    Fix cache busting for pack images

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      exclude: ['/cards/']
+      exclude: ['/cards/', 'standard-pack.png', 'premium-pack.png']
     },
     sassOptions: {
       includePaths: [
