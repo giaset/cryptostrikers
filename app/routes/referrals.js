b2f0fcaceb354035279fb853316b63bd48fb4466
Gianni Settino <g@miltonandparc.com>
2018-06-05 15:58:42 -0400

    Adds referrals-cards component

--- app/routes/referrals.js
+++ app/routes/referrals.js
@@ -2,17 +2,26 @@ import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 import { inject as service } from '@ember/service';
 
+const BONUS_CHECKLIST_IDS = [
+  115, 127, 122, 130,
+  116, 123, 121, 131
+];
+
 export default Route.extend({
   currentUser: service(),
   strikersContracts: service(),
 
   model() {
-    const user = this.get('currentUser.user');
     const contract = this.get('strikersContracts.StrikersPackSale');
+    const store = this.get('store');
+    const user = this.get('currentUser.user');
+
+    const bonusChecklistItems = BONUS_CHECKLIST_IDS.map(checklistId => store.findRecord('checklist-item', checklistId));
     const packsBought = contract.methods.packsBought(user.get('id')).call();
     const referralCode = user.get('referralCode');
 
     return RSVP.hash({
+      bonusChecklistItems: RSVP.all(bonusChecklistItems),
       packsBought,
       referralCode
     });
