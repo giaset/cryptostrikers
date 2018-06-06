5c34d10660ac176968d3d9fce32ffdcfec320a32
Gianni Settino <g@miltonandparc.com>
2018-06-06 10:21:30 -0400

    Navbar redesign

--- app/components/navigation-bar.js
+++ app/components/navigation-bar.js
@@ -4,6 +4,6 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   currentUser: service(),
   session: service(),
-  classNames: ['navbar', 'navbar-expand-md', 'navbar-light'],
-  tagName: 'header'
+  classNames: ['navigation-bar', 'navbar', 'navbar-expand-md', 'navbar-light'],
+  tagName: 'nav'
 });
