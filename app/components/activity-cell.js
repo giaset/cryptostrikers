d9a830f2cc7dcfa0a234d8b04525f262da7af924
Gianni Settino <g@miltonandparc.com>
2018-06-10 11:30:53 -0400

    Styles Activity route

--- app/components/activity-cell.js
+++ app/components/activity-cell.js
@@ -1,5 +1,35 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+const BUY_TYPES = {
+  buy_pack: true,
+  buy_pack_with_kitty: true
+};
+
+const TRADE_TYPES = {
+  cancel_trade: true,
+  create_trade: true,
+  fill_trade: true
+};
 
 export default Component.extend({
-  classNames: ['activity-cell', 'my-3', 'p-3', 'rounded', 'border', 'box-shadow']
+  classNames: ['activity-cell'],
+
+  activityImage: computed('activity.type', function() {
+    const type = this.get('activity.type');
+    let image = 'sell';
+
+    if (BUY_TYPES[type]) {
+      image = 'buy';
+    } else if (TRADE_TYPES[type]) {
+      image = 'trade';
+    }
+
+    return `/assets/images/activity-${image}.svg`;
+  }),
+
+  showViewCards: computed('activity.type', function() {
+    const type = this.get('activity.type');
+    return BUY_TYPES[type];
+  })
 });
