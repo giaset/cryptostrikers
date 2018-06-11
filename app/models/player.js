d8ddd29d56ddceb2d6ed6f799bddcf24d416c173
Gianni Settino <g@miltonandparc.com>
2018-06-11 09:07:34 -0400

    Player bios!

--- app/models/player.js
+++ app/models/player.js
@@ -1,6 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  bio: DS.attr('string'),
   country: DS.belongsTo('country', { inverse: null }),
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  wcInfo: DS.attr('string'),
+  wiki: DS.attr('string')
 });
