fbabe96f1821dbe68333a6e9de6f8073b30c8b7b
Gianni Settino <g@miltonandparc.com>
2018-06-17 00:28:00 -0400

    Fix intermittent Infura bug

--- app/controllers/activity/show.js
+++ app/controllers/activity/show.js
@@ -6,20 +6,24 @@ import { task, timeout } from 'ember-concurrency';
 export default Controller.extend({
   currentUser: service(),
   strikersContracts: service(),
-  web3: service(),
 
   getCardsFromTransaction: task(function * () {
     const contract = this.get('strikersContracts.StrikersPackSale');
     const filter = { buyer: this.get('currentUser.address') };
-    const events = yield contract.getPastEvents('PackBought', { filter, fromBlock: 0 });
-    const hash = this.get('model.activity.txnHash');
-    const event = events.findBy('transactionHash', hash);
-    if (event) {
-      const cardIds = event.returnValues.pack;
-      const cards = yield this._loadCards(cardIds);
-      this.set('cards', cards);
-    } else {
-      this.set('firstCheckDone', true);
+    try {
+      const events = yield contract.getPastEvents('PackBought', { filter, fromBlock: 0 });
+      const hash = this.get('model.activity.txnHash');
+      const event = events.findBy('transactionHash', hash);
+      if (event) {
+        const cardIds = event.returnValues.pack;
+        const cards = yield this._loadCards(cardIds);
+        this.set('cards', cards);
+      } else {
+        this.set('firstCheckDone', true);
+        yield timeout(1000);
+        this.get('getCardsFromTransaction').perform();
+      }
+    } catch(error) {
       yield timeout(1000);
       this.get('getCardsFromTransaction').perform();
     }
