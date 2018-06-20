b868c8d4d7525e5741b36066452cc2677a586853
Gianni Settino <g@miltonandparc.com>
2018-06-20 10:36:22 -0400

    Pack generator now works well!

--- helpers/new-pack-generator.js
+++ helpers/new-pack-generator.js
@@ -1,20 +1,24 @@
-let cardCounts;
+const fs = require('fs');
+
+const FEATURED_CHECKLIST_ID = 110;
+
+let cards;
 while (true) {
-  cardCounts = { 106: 100 };
+  cards = [];
   for (let i = 0; i < 50; i++) {
-    if (i < 4) {
-      cardCounts[i] = 19;
-    } else if (i < 20) {
-      cardCounts[i] = 24;
-    } else if (i < 50) {
-      cardCounts[i] = 48;
+    const count = countForChecklistId(i);
+    for (let j = 0; j < count; j++) {
+      cards.push(i);
     }
+  }
+
+  for (let i = 0; i < 100; i++) {
+    cards.push(FEATURED_CHECKLIST_ID);
   }
 
   const packs = [];
   for (let i = 0; i < 500; i++) {
-    const remainingCards = Object.keys(cardCounts);
-    const pack = newPack(remainingCards);
+    const pack = newPack();
 
     if (!pack) {
       break;
@@ -23,37 +27,47 @@ while (true) {
     packs.push(pack);
   }
 
-  if (packs.length > 495) {
-    console.log(cardCounts);
+  if (packs.length === 500) {
     packs.forEach(pack => {
       console.log(pack);
     });
+    //fs.writeFileSync('./helpers/fuck.json', JSON.stringify(packs));
+    break;
   }
 }
 
-function newPack(remainingCards) {
+function newPack() {
   const pack = [];
   let timeout = 0;
   while (true) {
     timeout++;
-    if (timeout > 100) { return null };
-    const index = Math.floor(Math.random() * remainingCards.length);
-    const card = remainingCards[index];
-
-    if (cardCounts[card] && cardCounts[card] > 0) {
-      const intCard = parseInt(card);
-      if (!pack.includes(intCard)) {
-        pack.push(intCard);
-        if (cardCounts[card] === 1) {
-          cardCounts[card] = null;
-        } else {
-          cardCounts[card]--;
-        }
-      }
-
-      if (pack.length === 4) {
-        return pack;
-      }
+    if (timeout > 100) {
+      // Put the cards back
+      pack.forEach(card => {
+        cards.push(card);
+      });
+      return null;
     }
+    const index = Math.floor(Math.random() * cards.length);
+    const card = cards[index];
+
+    if (!pack.includes(card)) {
+      cards.splice(index, 1);
+      pack.push(card);
+    }
+
+    if (pack.length === 4) {
+      return pack;
+    }
+  }
+}
+
+function countForChecklistId(id) {
+  if (id < 4) {
+    return 19;
+  } else if (id < 20) {
+    return 24;
+  } else if (id < 50) {
+    return 48;
   }
 }
