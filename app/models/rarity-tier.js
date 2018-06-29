e22f04af83902b733632acb38357175b7c04ad92
Gianni Settino <g@miltonandparc.com>
2018-06-29 15:34:19 +0200

    Translates My Cards

--- app/models/rarity-tier.js
+++ app/models/rarity-tier.js
@@ -1,6 +1,20 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default DS.Model.extend({
+  intl: service(),
+
+  chineseName: DS.attr('string'),
   limit: DS.attr('number'),
-  name: DS.attr('string')
+  name: DS.attr('string'),
+
+  localizedName: computed('intl.locale', 'chineseName', 'name', function() {
+    const locale = this.get('intl.locale')[0];
+    if (locale === 'zh-hans-cn') {
+      return this.get('chineseName');
+    }
+
+    return this.get('name');
+  })
 });
