58793d0ee0f749ab2dc29fbd04082d139d1e6772
Gianni Settino <g@miltonandparc.com>
2018-06-28 22:02:32 -0400

    Progress on translation

--- app/models/checklist-set.js
+++ app/models/checklist-set.js
@@ -1,5 +1,19 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  intl: service(),
+
+  chineseName: DS.attr('string'),
+  name: DS.attr('string'),
+
+  localizedName: computed('intl.locale', 'chineseName', 'name', function() {
+    const locale = this.get('intl.locale')[0];
+    if (locale === 'zh-hans-cn') {
+      return this.get('chineseName');
+    }
+
+    return this.get('name');
+  })
 });
--- app/models/player.js
+++ app/models/player.js
@@ -1,9 +1,23 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default DS.Model.extend({
+  intl: service(),
+
   bio: DS.attr('string'),
+  chineseName: DS.attr('string'),
   country: DS.belongsTo('country', { inverse: null }),
   name: DS.attr('string'),
   wcInfo: DS.attr('string'),
-  wiki: DS.attr('string')
+  wiki: DS.attr('string'),
+
+  localizedName: computed('intl.locale', 'chineseName', 'name', function() {
+    const locale = this.get('intl.locale')[0];
+    if (locale === 'zh-hans-cn') {
+      return this.get('chineseName');
+    }
+
+    return this.get('name');
+  })
 });
