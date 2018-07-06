8f4de949bc1681aaca6d0b993d093affba5c7188
Gianni Settino <g@miltonandparc.com>
2018-07-06 07:36:08 -0400

    Exclude flags from fingerprinting

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     fingerprint: {
-      exclude: ['/cards/', 'standard-pack.png', 'premium-pack.png']
+      exclude: ['/cards/', 'standard-pack.png', 'premium-pack.png', '/flags/']
     },
     sassOptions: {
       includePaths: [
